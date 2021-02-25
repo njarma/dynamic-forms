cc45b563131b59027cf974b499eefef1fe42d2a7
Nicolás Jarma <nicolasjarma@gmail.com>
2021-02-25 10:32:03 +0100

    creación estructura de componentes

---
+++ src/app/shared/directives/dynamic-form-input.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DynamicFormInputDirective } from './dynamic-form-input.directive';
+
+describe('DynamicFormInputDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DynamicFormInputDirective();
+    expect(directive).toBeTruthy();
+  });
+});
